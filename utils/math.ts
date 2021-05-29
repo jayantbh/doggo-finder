d07b7db1ea4e2451b9112d03ff796dd26a887269
Jayant Bhawal <bhawal.jayant@gmail.com>
2021-05-29 19:29:47 +0530

    Add randomInt util

--- utils/math.ts
+++ utils/math.ts
@@ -3,3 +3,6 @@ export const clamp = (value: number, min: number, max: number) => {
   if (value > max) return max;
   return value;
 };
+
+export const randomInt = (min = 0, max = 100) =>
+  Math.round(Math.random() * (max - min) + min);
